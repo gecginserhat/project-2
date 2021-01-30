133698a38d2902d87a773b8538359ce9b938e0ea
gecginserhat <74310970+gecginserhat@users.noreply.github.com>
2021-01-31 00:09:24 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace kendiçalışma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety- tip güvenliği
+            //Do not repeat yourself- KENDİNİ TEKRARLAMA
+
+            string kategoriEtiketi = "Kategori";
+
+            
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemGirisYapmısMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir Butonu");
+            }
+
+            if (sistemGirisYapmısMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
